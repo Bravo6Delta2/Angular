e6d9e4789718f10c00f7a0d560abb253e988a684
Bravo6Delta2 <aco.asanovski@gmail.com>
2023-05-06 11:06:00 +0200

    Day 5

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,6 +1,27 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
+interface User {
+  firstName: string
+  lastName: string
+  email: string
+  phoneNumber: string
+}
+interface Rent {
+  _id: string
+  userId: string
+  carId: string
+  start: Date
+  end: Date
+  price: number
+}
+
+interface ResT {
+  message: string
+  data: User
+  rents: [Rent]
+}
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -8,8 +29,23 @@ import {HttpClient} from "@angular/common/http";
 })
 export class ProfileComponent implements OnInit {
 
+  state = 0
+  user!: User
+  rents!: [Rent]
+  total!: number
   constructor(private http: HttpClient) { }
   ngOnInit(): void {
-  }
 
+    this.http.get('http://localhost:3001/user').subscribe(res => {
+      let result = res as ResT
+      this.user = result.data
+      this.rents = result.rents
+      this.state = 1
+
+      this.total = this.rents.reduce((accumulator, object) => {
+        return accumulator + object.price;
+      }, 0);
+    })
+
+  }
 }
